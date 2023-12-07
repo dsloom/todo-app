f8d396c6e9eef502fc64223d1abbff1b36e80835
prithvy <prithvykrishna67@gmail.com>
2023-12-07 15:59:41 +0100

    implemented the edit button

--- gui.py
+++ gui.py
@@ -5,25 +5,39 @@ import PySimpleGUI as sg
 windows is a type (like list)
 """
 label = sg.Text("Type in a to do")
-
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-
 add_button = sg.Button("Add")
-window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button]],
-                   font=("Helvetica", 20))
+listbox = sg.Listbox(values=functions.get_todos(), key="todos",
+                     enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
+
+
+window = sg.Window("My To-Do App",layout=[[label], [input_box, add_button],
+                    [listbox, edit_button]], font=("Helvetica", 20))
 
 while True:
 
     event, values = window.read()
     print(event)
-    print(values["todo"])
+    print(values)
     match event :
         case "Add":
             todos = functions.get_todos()
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+        case "Edit":
+            todos = functions.get_todos()
+            todo_to_edit = values["todos"][0]
+            index = todos.index(todo_to_edit)
+            new_todo = values["todo"] + "\n"
+            todos[index] = new_todo
+            functions.write_todos(todos)
+#for real time change of items in the list
+            window["todos"].update(todos)
+#whenever we select someting we have to see it on input box.
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
         case sg.WINDOW_CLOSED:
             break
 window.close()
